58c1a0aeb3e22124fc55dfc7462cae21b9db5b69
Thejasbshetty <thejasbshetty@gmail.com>
2023-11-09 11:29:01 +0530

    Your commit message

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css"; // Import external CSS
+
+const HomePage = () => {
+  const [textIndex, setTextIndex] = useState(0);
+  const [animationStyle, setAnimationStyle] = useState("typing");
+
+  useEffect(() => {
+    const text =
+      "Welcome to Git-Linux-Junction, your hub for mastering\n Git and Linux. Dive into tutorials and resources to explore \nversion control and open-source computing together!";
+    const textContainer = document.querySelector(".animated-text");
+    const textArray = text.split("");
+    if (textIndex < textArray.length) {
+      setTimeout(() => {
+        textContainer.textContent += textArray[textIndex];
+        setTextIndex((prevState) => prevState + 1);
+      }, 75); // Adjusted delay for faster text animation
+    } else {
+      const animationStyles = ["typing", "bouncing", "flashing"];
+      const randomIndex = Math.floor(Math.random() * animationStyles.length);
+      const newAnimationStyle = animationStyles[randomIndex];
+
+      setAnimationStyle(newAnimationStyle);
+      setTimeout(() => {
+        setTextIndex(0);
+        textContainer.textContent = "";
+      }, 2000);
+    }
+  }, [textIndex, animationStyle]);
+
+  return (
+    <div className="HomePage">
+      <header className={`HomePage-header ${animationStyle}`}>
+        <div
+          className="git-linux-junction fade-in"
+          style={{ color: "#bdc3c7", marginBottom: "20px" }}
+        >
+          GIT-LINUX-JUNCTION
+        </div>
+        <div className="animated-text"></div>
+        <div className="buttons-container">
+          <Link to="/git" className="button git">
+            Explore Git
+          </Link>
+          <Link to="/linux" className="button linux">
+            Discover Linux
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default HomePage;
